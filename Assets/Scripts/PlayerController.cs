3de0687b55913614007378076739eb770758e930
That_Guy977 <sirawit.andy977@gmail.com>
2022-11-17 16:04:33 +0700

    Add _Char sprites and animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,46 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpVel = 5.0f;
+    public float spriteScale = 0.2f;
+    public bool spriteFacingRight = true;
 
     new Rigidbody2D rigidbody;
+    GameObject sprite;
+    Animator animator;
+    bool jumping;
 
     void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        sprite = transform.Find("Sprite").gameObject;
+        animator = sprite.GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        sprite.transform.localScale = new(spriteScale * (spriteFacingRight ? 1 : -1), spriteScale, 1);
     }
 
     void Update()
     {
         Vector2 vel = rigidbody.velocity;
-        vel.x = Input.GetAxis("Move") * speed;
+        float move = Input.GetAxis("Move");
+        vel.x = move * speed;
+        if (move != 0)
+        {
+            sprite.transform.localScale = new(spriteScale * (spriteFacingRight ? 1 : -1) * Mathf.Sign(move), spriteScale, 1);
+        }
+        animator.SetBool("Moving", move != 0);
         if (Input.GetButtonDown("Jump") && CanJump())
         {
             vel.y += jumpVel;
+            jumping = true;
+            animator.SetBool("Jumping", true);
+        }
+        else if (jumping && CanJump())
+        {
+            jumping = false;
+            animator.SetBool("Jumping", false);
         }
         rigidbody.velocity = vel;
     }
